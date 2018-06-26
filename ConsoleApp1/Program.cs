a5e4f63a6f2375198d49d28ce60fbe44f6742e6d
Jaszczab <23362384+Jaszczab@users.noreply.github.com>
2018-06-26 20:44:30 +0200

    Program Podaje pełną datę bez zbędnego czasu i jest bardziej przejrzysty :)

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -49,21 +49,23 @@ namespace KonsolaCzasu
             int choosedFunc = Int32.Parse(chooseFunc);
             if (choosedFunc == 1)
             {
-                Console.WriteLine("Geave me date first: (year/month/day) and then number of days in future to go(1 - 99999). ");
+                Console.WriteLine("Give me date first: (year/month/day) and then number of days in future to go");
                 var a = Console.ReadLine();
+                Console.WriteLine("Now Set a number of days from 1 to 99999");
                 var b = Console.ReadLine();
                 int be = Int32.Parse(b);
                 var ready1 = AddDays(a, be);
-                var got =  ready1.ToString();
+                var got = ready1.ToLongDateString();
                 Console.WriteLine(got);
             }
             else if (choosedFunc == 2)
             {
-                Console.WriteLine("Geave me date first: (year/month/day) and then number of days in past to go(1 - 99999). ");
+                Console.WriteLine("Give me date first: (year/month/day) and then number of days in past to go");
                 var a = Console.ReadLine();
+                Console.WriteLine("Now Set a number of days from 1 to 99999");
                 var b = Console.ReadLine();
                 var ready2 = RemoveDays(a, b);
-                var got1 =  ready2.ToString();
+                var got1 = ready2.ToLongDateString();
                 Console.WriteLine(got1);
             }
             else
